db4b0b824c2f3a2fe29812132fa35425e0b5b81d
angeldvilla <escarlata.2003@gmail.com>
2023-09-25 15:34:54 -0500

    feature /skills

--- src/handlers/allSkillsHandler.js
+++ src/handlers/allSkillsHandler.js
@@ -2,23 +2,21 @@ const skillsData = require("../../data/skills.json");
 
 module.exports = async (req, res) => {
   try {
-    const softSkills = skillsData.map((softSk) => ({
+
+    const soft = skillsData.softSkills.map((softSk) => ({
       id: softSk.id,
       title: softSk.title,
       description: softSk.description,
       image: softSk.image,
-    }));
-
-    const techSkills = skillsData.map((techSk) => ({
+    }))
+    
+    const tech = skillsData.techSkills.map((techSk) => ({
       id: techSk.id,
       title: techSk.title,
       image: techSk.image,
     }));
-
-    return res.status(200).json({
-      softSkills: softSkills,
-      techSkills: techSkills,
-    });
+    return res.status(200).json( {soft, tech} );
+    
   } catch (error) {
     return res.status(500).json(error);
   }
